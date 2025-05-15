a66f488f5a9bcc5f572bb43a33d0b1e1836614a8
Kowshik Valipireddy <139107793+kowshik3383@users.noreply.github.com>
2025-05-15 17:53:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -553,4 +553,6 @@ app.put("/tickets/:id", (req, res) => {
   });
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
